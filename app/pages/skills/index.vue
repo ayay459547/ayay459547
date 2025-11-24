d858406d77339f8e5d99b26bd226eaf510020f2f
ayay459547 <ayay459547@gmail.com>
2025-11-24 21:50:49 +0800

    feat: add pages content

--- app/pages/skills/index.vue
+++ app/pages/skills/index.vue
@@ -1,7 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SkillsSvg from '~/assets/svg/Professional-Skills.svg?url'
+
+</script>
 
 <template>
-  <div>Professional Skills</div>
+  <div class="skills-page">
+    <h3>專業技能 Professional Skills</h3>
+
+    <ul>
+      <li>Vue</li>
+      <li>React</li>
+      <li>JavaScript (TypeScript)</li>
+      <li>HTML</li>
+      <li>CSS (SCSS)</li>
+    </ul>
+
+    <img :src="SkillsSvg" alt="svg" />
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.skills-page {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  position: relative;
+  padding: 2rem;
+  font-size: 1.3rem;
+
+  ul {
+    font-size: 1.5rem;
+  }
+
+  img {
+    width: 240px;
+    height: auto;
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+
+    @media screen and (max-width: 768px) {
+      width: 160px;
+    }
+  }
+}
+</style>
